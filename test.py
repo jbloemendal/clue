e8d319cbac575417352c89a19e9c2fb9d10532d8
Jannis Bloemendal <j.bloemendal@posteo.de>
2018-11-29 16:58:39 +0100

    qn added

--- test.py
+++ test.py
@@ -15,11 +15,16 @@ class TestCCodeParser(unittest.TestCase):
         parser = CCodeParser(ccode)
         parser.parse()
         self.assertEqual(2, parser.cabe())
-    def test_colloc1_uniquePath(self):
+    def test_colloc1_subPaths(self):
         ccode = 'if (a>-1) { }'
         parser = CCodeParser(ccode)
         parser.parse()
-        self.assertEqual(2, parser.uniquePath())
+        self.assertEqual(2, parser.subPaths())
+    def test_colloc1_subPathsEnclosed(self):
+        ccode = 'if (a>-1) { }'
+        parser = CCodeParser(ccode)
+        parser.parse()
+        self.assertEqual(2, parser.subPathsEnclosed())
 
     # colloc 2
     def test_colloc_2_acyc(self):
@@ -32,11 +37,17 @@ class TestCCodeParser(unittest.TestCase):
         parser = CCodeParser(ccode)
         parser.parse()
         self.assertEqual(3, parser.cabe())
-    def test_colloc_2_uniquePath(self):
+    def test_colloc_2_subPaths(self):
         ccode = 'if (b<3 && c>4) { }'
         parser = CCodeParser(ccode)
         parser.parse()
-        self.assertEqual(3, parser.uniquePath())
+        self.assertEqual(3, parser.subPaths())
+    def test_colloc_2_subPathsEnclosed(self):
+        ccode = 'if (b<3 && c>4) { }'
+        parser = CCodeParser(ccode)
+        parser.parse()
+        self.assertEqual(3, parser.subPathsEnclosed())
+
 
     # colloc 3
     def test_colloc3_acyc(self):
@@ -49,11 +60,16 @@ class TestCCodeParser(unittest.TestCase):
         parser = CCodeParser(ccode)
         parser.parse()
         self.assertEqual(3, parser.cabe())
-    def test_colloc3_uniquePath(self):
+    def test_colloc3_subPaths(self):
         ccode = 'if (b<3 || c>4) { }'
         parser = CCodeParser(ccode)
         parser.parse()
-        self.assertEqual(3, parser.uniquePath())
+        self.assertEqual(3, parser.subPaths())
+    def test_colloc3_subPathsEnclosed(self):
+        ccode = 'if (b<3 || c>4) { }'
+        parser = CCodeParser(ccode)
+        parser.parse()
+        self.assertEqual(3, parser.subPathsEnclosed())
 
     # colloc 4
     def test_colloc4_acyc(self):
@@ -66,11 +82,16 @@ class TestCCodeParser(unittest.TestCase):
         parser = CCodeParser(ccode)
         parser.parse()
         self.assertEqual(3, parser.cabe())
-    def test_colloc4_uniquePath(self):
+    def test_colloc4_subPaths(self):
         ccode = 'if (d>0) { if(e>3){} }'
         parser = CCodeParser(ccode)
         parser.parse()
-        self.assertEqual(3, parser.uniquePath())
+        self.assertEqual(3, parser.subPaths())
+    def test_colloc4_subPathsEnclosed(self):
+        ccode = 'if (d>0) { if(e>3){} }'
+        parser = CCodeParser(ccode)
+        parser.parse()
+        self.assertEqual(3, parser.subPathsEnclosed())
 
     # colloc 5
     def test_colloc5_acyc(self):
@@ -83,11 +104,16 @@ class TestCCodeParser(unittest.TestCase):
         parser = CCodeParser(ccode)
         parser.parse()
         self.assertEqual(3, parser.cabe())
-    def test_colloc5_uniquePath(self):
+    def test_colloc5_subPaths(self):
         ccode = 'if (f>2) {} if(g<6) {} '
         parser = CCodeParser(ccode)
         parser.parse()
-        self.assertEqual(4, parser.uniquePath())
+        self.assertEqual(4, parser.subPaths())
+    def test_colloc5_subPathsEnclosed(self):
+        ccode = 'if (f>2) {} if(g<6) {} '
+        parser = CCodeParser(ccode)
+        parser.parse()
+        self.assertEqual(4, parser.subPathsEnclosed())
 
     # colloc 6
     def test_colloc6_acyc(self):
@@ -100,11 +126,16 @@ class TestCCodeParser(unittest.TestCase):
         parser = CCodeParser(ccode)
         parser.parse()
         self.assertEqual(4, parser.cabe()) 
-    def test_colloc6_uniquePath(self):
+    def test_colloc6_subPaths(self):
         ccode = 'if (h>2 && i<3 && j>10) { }'
         parser = CCodeParser(ccode)
         parser.parse()
-        self.assertEqual(4, parser.uniquePath()) 
+        self.assertEqual(4, parser.subPaths()) 
+    def test_colloc6_subPathsEnclosed(self):
+        ccode = 'if (h>2 && i<3 && j>10) { }'
+        parser = CCodeParser(ccode)
+        parser.parse()
+        self.assertEqual(4, parser.subPathsEnclosed()) 
 
     # colloc 7
     def test_colloc7_acyc(self):
@@ -117,11 +148,16 @@ class TestCCodeParser(unittest.TestCase):
         parser = CCodeParser(ccode)
         parser.parse()
         self.assertEqual(4, parser.cabe()) 
-    def test_colloc7_uniquePath(self):
+    def test_colloc7_subPaths(self):
         ccode = 'if (k>8 && i<5) { if (m<11) {} }'
         parser = CCodeParser(ccode)
         parser.parse()
-        self.assertEqual(4, parser.uniquePath()) 
+        self.assertEqual(4, parser.subPaths()) 
+    def test_colloc7_subPathsEnclosed(self):
+        ccode = 'if (k>8 && i<5) { if (m<11) {} }'
+        parser = CCodeParser(ccode)
+        parser.parse()
+        self.assertEqual(4, parser.subPathsEnclosed()) 
 
     # colloc 8 
     def test_colloc71_acyc(self):
@@ -134,11 +170,16 @@ class TestCCodeParser(unittest.TestCase):
         parser = CCodeParser(ccode)
         parser.parse()
         self.assertEqual(4, parser.cabe()) 
-    def test_colloc7_uniquePath(self):
+    def test_colloc7_subPaths(self):
         ccode = 'if (k>8 || i<5) { if (m<11) {} }'
         parser = CCodeParser(ccode)
         parser.parse()
-        self.assertEqual(5, parser.uniquePath()) 
+        self.assertEqual(5, parser.subPaths()) 
+    def test_colloc7_subPathsEnclosed(self):
+        ccode = 'if (k>8 || i<5) { if (m<11) {} }'
+        parser = CCodeParser(ccode)
+        parser.parse()
+        self.assertEqual(4, parser.subPathsEnclosed()) 
 
     # colloc 9
     def test_colloc9_acyc(self):
@@ -151,11 +192,16 @@ class TestCCodeParser(unittest.TestCase):
         parser = CCodeParser(ccode)
         parser.parse()
         self.assertEqual(4, parser.cabe()) 
-    def test_colloc9_uniquePath(self):
+    def test_colloc9_subPaths(self):
         ccode = 'if (n>0) { if (o<3) {} if (p<12){} }'
         parser = CCodeParser(ccode)
         parser.parse()
-        self.assertEqual(5, parser.uniquePath()) 
+        self.assertEqual(5, parser.subPaths()) 
+    def test_colloc9_subPathsEnclosed(self):
+        ccode = 'if (n>0) { if (o<3) {} if (p<12){} }'
+        parser = CCodeParser(ccode)
+        parser.parse()
+        self.assertEqual(5, parser.subPathsEnclosed()) 
 
     # colloc 10
     def test_colloc10_acyc(self):
@@ -168,11 +214,16 @@ class TestCCodeParser(unittest.TestCase):
         parser = CCodeParser(ccode)
         parser.parse()
         self.assertEqual(4, parser.cabe()) 
-    def test_colloc10_uniquePath(self):
+    def test_colloc10_subPaths(self):
         ccode = 'if (q>2) { if (r<3) { if (s<3){} } }'
         parser = CCodeParser(ccode)
         parser.parse()
-        self.assertEqual(4, parser.uniquePath()) 
+        self.assertEqual(4, parser.subPaths()) 
+    def test_colloc10_subPathsEnclosed(self):
+        ccode = 'if (q>2) { if (r<3) { if (s<3){} } }'
+        parser = CCodeParser(ccode)
+        parser.parse()
+        self.assertEqual(4, parser.subPathsEnclosed()) 
 
     # colloc 11 
     def test_colloc11_acyc(self):
@@ -185,11 +236,16 @@ class TestCCodeParser(unittest.TestCase):
         parser = CCodeParser(ccode)
         parser.parse()
         self.assertEqual(4, parser.cabe()) 
-    def test_colloc11_uniquePath(self):
+    def test_colloc11_subPaths(self):
         ccode = 'if (t>2) { if (u>10) { } } if (v>4){ } '
         parser = CCodeParser(ccode)
         parser.parse()
-        self.assertEqual(6, parser.uniquePath()) 
+        self.assertEqual(6, parser.subPaths()) 
+    def test_colloc11_subPathsEnclosed(self):
+        ccode = 'if (t>2) { if (u>10) { } } if (v>4){ } '
+        parser = CCodeParser(ccode)
+        parser.parse()
+        self.assertEqual(5, parser.subPathsEnclosed()) 
 
     # colloc 12 
     def test_colloc12_acyc(self):
@@ -202,11 +258,16 @@ class TestCCodeParser(unittest.TestCase):
         parser = CCodeParser(ccode)
         parser.parse()
         self.assertEqual(4, parser.cabe()) 
-    def test_colloc12_uniquePath(self):
+    def test_colloc12_subPaths(self):
         ccode = 'if (w>2) { } if (x>5) { } if (y>3){ } '
         parser = CCodeParser(ccode)
         parser.parse()
-        self.assertEqual(8, parser.uniquePath()) 
+        self.assertEqual(8, parser.subPaths()) 
+    def test_colloc12_subPathsEnclosed(self):
+        ccode = 'if (w>2) { } if (x>5) { } if (y>3){ } '
+        parser = CCodeParser(ccode)
+        parser.parse()
+        self.assertEqual(6, parser.subPathsEnclosed()) 
 
 
 
@@ -220,11 +281,11 @@ class TestCCodeParser(unittest.TestCase):
         parser = CCodeParser(ccode)
         parser.parse()
         self.assertEqual(5, parser.cabe()) 
-    def test_43n_uniquePath(self):
+    def test_43n_subPaths(self):
         ccode = 'if (a) { if (b){} if(c){} if(d){} }'
         parser = CCodeParser(ccode)
         parser.parse()
-        self.assertEqual(9, parser.uniquePath()) 
+        self.assertEqual(9, parser.subPaths()) 
 
     
     def test_41n_acyc(self):
@@ -237,11 +298,11 @@ class TestCCodeParser(unittest.TestCase):
         parser = CCodeParser(ccode)
         parser.parse()
         self.assertEqual(5, parser.cabe()) 
-    def test_41n_uniquePath(self):
+    def test_41n_subPaths(self):
         ccode = 'if (a && (b || c)) { if (d) { } }'
         parser = CCodeParser(ccode)
         parser.parse()
-        self.assertEqual(6, parser.uniquePath()) 
+        self.assertEqual(6, parser.subPaths()) 
 
 
     def test_52n_acyc(self):
@@ -254,11 +315,11 @@ class TestCCodeParser(unittest.TestCase):
         parser = CCodeParser(ccode)
         parser.parse()
         self.assertEqual(6, parser.cabe()) 
-    def test_52n_uniquePath(self):
+    def test_52n_subPaths(self):
         ccode = 'if (a && (b || c)) { if (d) { } if (e) {} }'
         parser = CCodeParser(ccode)
         parser.parse()
-        self.assertEqual(10, parser.uniquePath()) 
+        self.assertEqual(10, parser.subPaths()) 
 
 
 
@@ -272,7 +333,7 @@ class TestCCodeParser(unittest.TestCase):
         parser = CCodeParser(ccode)
         parser.parse()
         self.assertEqual(2, parser.cabe())
-    def test_for_uniquePath(self):
+    def test_for_subPaths(self):
         ccode = 'for (int i; i<=9; i++) { }'
         parser = CCodeParser(ccode)
         parser.parse()
@@ -289,11 +350,11 @@ class TestCCodeParser(unittest.TestCase):
         parser = CCodeParser(ccode)
         parser.parse()
         self.assertEqual(2, parser.cabe())
-    def test_while_uniquePath(self):
+    def test_while_subPaths(self):
         ccode = 'while (i<=9) { }'
         parser = CCodeParser(ccode)
         parser.parse()
-        self.assertEqual(2, parser.uniquePath())
+        self.assertEqual(2, parser.subPaths())
 
 
     def test_folding_acyc(self):
@@ -387,7 +448,7 @@ class TestCCodeParser(unittest.TestCase):
         scopes = parser.parse()
         self.assertEqual(7, parser.cabe()) 
 
-    def test_example_uniquePath(self):
+    def test_example_subPaths(self):
         ccode = '''
         int size = image.length();
         StringBuilder buf = new StringBuilder(size);
@@ -407,7 +468,7 @@ class TestCCodeParser(unittest.TestCase):
         '''
         parser = CCodeParser(ccode)
         scopes = parser.parse()
-        self.assertEqual(7, parser.uniquePath()) 
+        self.assertEqual(7, parser.subPaths()) 
 
 if __name__ == '__main__':
     unittest.main()

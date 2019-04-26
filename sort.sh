1f51366600c5899fadecb0e9002c7a9b55747c77
Jannis Bloemendal <j.bloemendal@posteo.de>
2019-04-26 11:28:43 +0200

    renamed

--- sort.sh
+++ sort.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-for f in samples/*; do m=`cat $f | ./ahm`; echo "$m $f"; done | sort 
-#for f in samples/*; do m=`cat $f | ./ahm`; echo "$m $f"; done | sort | sed "s/^[0-9\s\.]* //"
+for f in samples/*; do m=`cat $f | ./mix`; echo "$m $f"; done | sort 
+#for f in samples/*; do m=`cat $f | ./mix`; echo "$m $f"; done | sort | sed "s/^[0-9\s\.]* //"

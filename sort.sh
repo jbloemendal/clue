f52a13614ea32dc38310fd5d882af4236a5579d4
Jannis Bloemendal <j.bloemendal@posteo.de>
2019-04-30 11:03:23 +0200

    renamed samples into examples

--- sort.sh
+++ sort.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-for f in samples/*; do m=`cat $f | ./ksi`; echo "$m $f"; done | sort 
-#for f in samples/*; do m=`cat $f | ./ksi`; echo "$m $f"; done | sort | sed "s/^[0-9\s\.]* //"
+for f in examples/*; do m=`cat $f | ./ksi`; echo "$m $f"; done | sort 
+#for f in examples/*; do m=`cat $f | ./ksi`; echo "$m $f"; done | sort | sed "s/^[0-9\s\.]* //"

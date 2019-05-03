ec91259514baf5c0c6aee9ed20b43ac825fd4da8
Jannis Bloemendal <j.bloemendal@posteo.de>
2019-05-04 00:52:11 +0200

    added example

--- sort.sh
+++ sort.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-for f in examples/*; do m=`cat $f | ./ksi`; echo "$m $f"; done | sort 
+#for f in examples/set1/*; do m=`cat $f | ./ksi`; echo "$m $f"; done | sort 
+#for f in examples/set2/*; do m=`cat $f | ./ksi`; echo "$m $f"; done | sort 
+for f in examples/set3/*; do c=`cat $f`; m=`cat $f | ./ksi`; echo "$m $f $c"; done | sort 
 #for f in examples/*; do m=`cat $f | ./ksi`; echo "$m $f"; done | sort | sed "s/^[0-9\s\.]* //"

a9ebe90f4f1958fa7bccfee9290db53d4b1e9c42
Jannis Bloemendal <j.bloemendal@posteo.de>
2019-05-10 17:08:41 +0200

    renamed ksi->kso

--- sort.sh
+++ sort.sh
@@ -3,4 +3,4 @@
 #for f in examples/set2/*; do m=`cat $f | ./ksi`; echo "$m $f"; done | sort 
 #for f in examples/set3/*; do c=`cat $f`; m=`cat $f | ./ksi`; echo "$m $f $c"; done | sort 
 #for f in examples/*; do m=`cat $f | ./ksi`; echo "$m $f"; done | sort | sed "s/^[0-9\s\.]* //"
-while read line; do c=`echo $line`; k=`echo $c | ./ksi`; echo "$k, $c"; done < examples/set4/set4.c | sort
+while read line; do c=`echo $line`; k=`echo $c | ./kso`; echo "$k, $c"; done < examples/set4/set4.c | sort

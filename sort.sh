ddf0a5cde23109674170bbb69c92ed8fb8f19383
Jannis Bloemendal <j.bloemendal@posteo.de>
2019-05-17 10:25:09 +0200

    improved overview

--- sort.sh
+++ sort.sh
@@ -3,4 +3,4 @@
 #for f in examples/set2/*; do m=`cat $f | ./clue`; echo "$m $f"; done | sort 
 #for f in examples/set3/*; do c=`cat $f`; m=`cat $f | ./clue`; echo "$m $f $c"; done | sort 
 #for f in examples/*; do m=`cat $f | ./clue`; echo "$m $f"; done | sort | sed "s/^[0-9\s\.]* //"
-while read line; do c=`echo $line`; k=`echo $c | ./clue`; echo "$k, $c"; done < examples/set4/set4.c | sort
+while read line; do c=`echo $line`; k=`echo $c | ./clue`; echo "$k, $c"; done < examples/set5/set5.c 

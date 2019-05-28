5a4fad29a8fbb318628dd172c21248d0504d103d
Jannis Bloemendal <j.bloemendal@posteo.de>
2019-05-28 09:57:18 +0200

    added examples set

---
+++ examples/set6/set6.c
@@ -0,0 +1,4 @@
+if (a && b || c) { if (d && e) {} }
+if (a && b || c) { if (d) { if (e) { } } }
+if (a) { if (b) { if (d && e) {} }} if (c) { if (d && e) {} } 
+if (a) { if (b) { if (d) { if (e)} }} if (c) { if (d) { if (e) {}} } }
